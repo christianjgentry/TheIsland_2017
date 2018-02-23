1b82f98817293dce5bb5b1f2e168c4e84776a668
christianjgentry <32580981+christianjgentry@users.noreply.github.com>
2018-02-22 21:45:44 -0600

    _1

---
+++ Assets/meshTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meshTest : MonoBehaviour {
+
+    public Mesh thisMesh;
+    public GameObject obj;
+
+
+	void Start () {
+        Vector3[] verts = thisMesh.vertices;
+
+        for (int i = 0; i < verts.Length; i++)
+        {
+            if (i % 4 == 0)
+            {
+                Instantiate(obj, verts[i] + transform.position, Quaternion.identity).transform.parent = gameObject.transform;
+            }
+        }
+	}
+}
